55ed4cccaebe3ddcb3228596dc3a81dccd26e10a
yarivar <yariv.ar@gmail.com>
2019-09-15 07:59:52 +0300

    var -> let.  Reuse code.

--- connect4.js
+++ connect4.js
@@ -1,9 +1,9 @@
-var _board = [];
-var _isCurrentPlayerRed = true;
-var _rowsNames = ['A', 'B', 'C', 'D', 'E', 'F'];
-var _rows = 6;
-var _columns = 7;
-var _players = ['red', 'yellow'];
+let _board = [];
+let _isCurrentPlayerRed = true;
+let _rowsNames = ['A', 'B', 'C', 'D', 'E', 'F'];
+let _rows = 6;
+let _columns = 7;
+let _players = ['red', 'yellow'];
 
 
 $(document).ready(function () {
@@ -26,7 +26,7 @@ $(document).ready(function () {
 
 function UpdateCurrentTurn() {
     _isCurrentPlayerRed = _isCurrentPlayerRed === true ? false : true;
-    var myColor = GetCurrentColor();
+    let myColor = GetCurrentColor();
     $(".currentTurn").css('background-color', myColor);
 }
 
@@ -35,9 +35,9 @@ function GetCurrentColor() {
 }
 
 function ResetBoard() {
-    for (var i = 0; i < _rows; i++) {
+    for (let i = 0; i < _rows; i++) {
         _board.push([0]);
-        for (var j = 0; j < _columns; j++) {
+        for (let j = 0; j < _columns; j++) {
             _board[i][j] = 0;
         }
     }
@@ -45,7 +45,7 @@ function ResetBoard() {
 }
 
 function UpdateBoard(myCellId) {
-    for (var i = 0; i < _rows; i++) {
+    for (let i = 0; i < _rows; i++) {
         if (_board[i][myCellId.substring(1) - 1] === 0) {
             _board[i][myCellId.substring(1) - 1] = _isCurrentPlayerRed === true ? 1 : 2;
             UpdateBoardGui();
@@ -56,17 +56,17 @@ function UpdateBoard(myCellId) {
 }
 
 function UpdateBoardGui() {
-    for (var i = 0; i < _rows; i++) {
-        for (var j = 0; j < _columns; j++) {
-            var myId = '#' + _rowsNames[i] + (j + 1);
-            var myColor = _board[i][j] === 0 ? "empty" : _board[i][j] === 1 ? "red" : "yellow";
+    for (let i = 0; i < _rows; i++) {
+        for (let j = 0; j < _columns; j++) {
+            let myId = '#' + _rowsNames[i] + (j + 1);
+            let myColor = _board[i][j] === 0 ? "empty" : _board[i][j] === 1 ? "red" : "yellow";
             $(myId).attr('src', 'images/' + myColor + '.jpg');
         }
     }
 }
 
 function IsGameOver() {
-    var isWin = false;
+    let isWin = false;
 
     isWin = CheckHorizontal();
 
@@ -75,22 +75,22 @@ function IsGameOver() {
     }
 
     if (!isWin) {
-        isWin = CheckDiagonalLtr();
+        isWin = CheckDiagonal(1);
     }
 
     if (!isWin) {
-        isWin = CheckDiagonalRtl();
+        isWin = CheckDiagonal(-1);
     }
 
     return isWin;
 }
 
 function CheckHorizontal() {
-    var isWin = false;
-    var myCounter = 0;
+    let isWin = false;
+    let myCounter = 0;
 
-    for (var i = _rows - 1; i >= 0; i--) {
-        for (var j = 0; j < _columns - 1; j++) {
+    for (let i = _rows - 1; i >= 0; i--) {
+        for (let j = 0; j < _columns - 1; j++) {
             if (_board[i][j] !== 0 && j < 4) {
                 for (myCounter = 1; myCounter < 5; myCounter++) {
                     if (_board[i][j] === _board[i][j + myCounter]) {
@@ -109,11 +109,11 @@ function CheckHorizontal() {
 }
 
 function CheckVertical() {
-    var isWin = false;
-    var myCounter = 0;
+    let isWin = false;
+    let myCounter = 0;
 
-    for (var j = 0; j < _columns; j++) {
-        for (var i = _rows - 1; i >= 3; i--) {
+    for (let j = 0; j < _columns; j++) {
+        for (let i = _rows - 1; i >= 3; i--) {
             if (_board[i][j] !== 0) {
                 for (myCounter = 1; myCounter < 5; myCounter++) {
                     if (_board[i][j] === _board[i - myCounter][j]) {
@@ -131,38 +131,15 @@ function CheckVertical() {
     return isWin;
 }
 
-function CheckDiagonalLtr() {
-    var isWin = false;
-    var myCounter = 0;
+function CheckDiagonal(ltr) {
+    let isWin = false;
+    let myCounter = 0;
 
-    for (var j = 0; j < _columns; j++) {
-        for (var i = _rows - 1; i >= 3; i--) {
+    for (let j = 0; j < _columns; j++) {
+        for (let i = _rows - 1; i >= 3; i--) {
             if (_board[i][j] !== 0) {
                 for (myCounter = 1; myCounter < 5; myCounter++) {
-                    if (_board[i][j] === _board[i - myCounter][j + myCounter]) {
-                        if (myCounter === 3) {
-                            return true;
-                        }
-                    }
-                    else {
-                        break;
-                    }
-                }
-            }
-        }
-    }
-    return isWin;
-}
-
-function CheckDiagonalRtl() {
-    var isWin = false;
-    var myCounter = 0;
-
-    for (var j = 0; j < _columns; j++) {
-        for (var i = _rows - 1; i >= 3; i--) {
-            if (_board[i][j] !== 0) {
-                for (myCounter = 1; myCounter < 5; myCounter++) {
-                    if (_board[i][j] === _board[i - myCounter][j - myCounter]) {
+                    if (_board[i][j] === _board[i - myCounter][j + ltr*myCounter]) {
                         if (myCounter === 3) {
                             return true;
                         }
@@ -179,7 +156,7 @@ function CheckDiagonalRtl() {
 
 function EndGame() {
     UpdateCurrentTurn();
-    var myColor = GetCurrentColor();
+    let myColor = GetCurrentColor();
 
     setTimeout(() => {
         alert("Game Over - " + myColor + " win!!!");
